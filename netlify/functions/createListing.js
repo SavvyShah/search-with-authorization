24b9efcccf9a5021c0cc85974ec8c475b7807a53
Shubham Shah <shubhamshahrising@gmail.com>
2022-05-30 22:42:34 +0530

    Create a listing by recieving form data

--- netlify/functions/createListing.js
+++ netlify/functions/createListing.js
@@ -1,31 +1,37 @@
 const Appbase = require("appbase-js");
+const ManagementClient = require("auth0").ManagementClient;
+
+const auth0 = new ManagementClient({
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+});
 
 exports.handler = async function (event, context) {
   try {
-    const appbaseRef = Appbase({
-      url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
-      app: "clone-airbeds",
-      credentials: process.env.APPBASE_API_CREDENTIAL,
-    });
-    const doc = await appbaseRef.index({
-      body: {
-        name: "Test airbeds",
-        host_name: "Mimi",
-        accomodates: 2,
-        bedrooms: 1,
-        date_from: new Date("2017-04-13"),
-        date_to: new Date("2017-05-14"),
-        price: 150,
-      },
-    });
-    return {
-      statusCode: 200,
-      body: JSON.stringify(doc),
-    };
-  } catch (err) {
+    const { user_id, ...body } = JSON.parse(event.body);
+    const roles = await auth0.getUserRoles({ id: user_id });
+    const isHost = roles.filter((role) => role.name === "host").length > 0;
+    if (isHost) {
+      const appbaseRef = Appbase({
+        url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
+        app: "clone-airbeds",
+        credentials: process.env.APPBASE_API_CREDENTIAL,
+      });
+      const doc = await appbaseRef.index({
+        body,
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(doc),
+      };
+    } else {
+      throw new Error("Unauthorized: User doesn't have necessary permission");
+    }
+  } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: err.message }),
+      body: JSON.stringify({ error: error.message }),
     };
   }
 };
