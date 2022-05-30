8d1085ea6c7a8c031dcfce7c964489df08394e3e
Shubham Shah <shubhamshahrising@gmail.com>
2022-05-31 00:16:25 +0530

    Add a loading spinner

---
+++ src/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./LoadingSpinner.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="loading">
+      <i className="fa fa-spinner loading__icon"></i>
+    </div>
+  );
+}
